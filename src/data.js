bb0e8dbba7250b2c585e45ce14a288f0e82a8229
Santiago Soto <stjamessoto@gmail.com>
2026-02-16 18:29:23 -0500

    edits

--- src/data.js
+++ src/data.js
@@ -2,13 +2,13 @@ export const initialData = [
   {
     id: 1,
     name: "Dog 1",
-    status: "Hungry",
-    image: "https://some-url.com/sad-yoda.jpg"
+    //status: "Hungry",
+    //image: "https://some-url.com/sad-yoda.jpg"
   },
   {
     id: 2,
     name: "Dog 2",
-    status: "Lost",
-    image: "https://some-url.com/lost-mando.jpg"
+    //status: "Lost",
+    //image: "https://some-url.com/lost-mando.jpg"
   }
 ];
